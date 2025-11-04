ae26c1dc4e3d0a985f305c2e6634fb666b37ec5c
eduardo-franca-projetos <eduardo.alcantara0905.projetos@gmail.com>
2025-11-04 09:15:01 -0300

    update script
    Atualização no script onde o código foi separado para melhor entendimento

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+function calcularAnimal(anoInserido) {
+    var res = document.getElementById('tela')
+
+    while (anoInserido > 11) {
+        anoInserido = anoInserido - 12
+    }
+    switch (anoInserido) {
+
+        case 0:
+            var animal = "Coelho"
+            break;
+        case 1:
+            var animal = "Dragão"
+            break;
+        case 2:
+            var animal = "Serpente"
+            break;
+        case 3:
+            var animal = "Cavalo"
+            break;
+        case 4:
+            var animal = "Cabra"
+            break;
+        case 5:
+            var animal = "Macaco"
+            break;
+        case 6:
+            var animal = "Galo"
+            break;
+        case 7:
+            var animal = "Cão"
+            break;
+        case 8:
+            var animal = "Porco"
+            break;
+        case 9:
+            var animal = "Rato"
+            break;
+        case 10:
+            var animal = "Boi"
+            break;
+        case 11:
+            var animal = "Tigre"
+            break;
+
+        default:
+            var animal = "Resultado Inválido"
+            break;
+
+    }
+    res.innerHTML = `Seu animal do Horóscopo zodíaco chinês é ${animal}!`;
+}
 function clicou() {
     var ano = document.getElementById('anonascimento')
     var ano = Number(ano.value)
@@ -5,66 +57,15 @@ function clicou() {
     var anoatual = new Date()
     var anoatual = anoatual.getFullYear()
     var anoLimite = 1927 //1987
-    
-    if (ano !== "" && ano < anoatual && ano >= anoLimite && Number.isInteger(ano) && ano > 0) {
 
-
+    if (ano < 1927) {
+        res.innerHTML = `Insira um ano maior ou igual a 1927`
+    } else if (ano > anoatual) {
+        res.innerHTML = `Insira um ano igual ou inferior a ${anoatual}`
+    } else if (!Number.isInteger(ano)) {
+        res.innerHTML = `Insira um número inteiro`
+    } else if (Number.isInteger(ano) || ano >= 1927 && ano <= anoatual) {
         var ano = ano - anoLimite
-
-        while (ano > 11) {
-            var ano = ano - 12
-        }
-        switch (ano) {
-
-            case 0:
-                var animal = "Coelho"
-                break;
-            case 1:
-                var animal = "Dragão"
-                break;
-            case 2:
-                var animal = "Serpente"
-                break;
-            case 3:
-                var animal = "Cavalo"
-                break;
-            case 4:
-                var animal = "Cabra"
-                break;
-            case 5:
-                var animal = "Macaco"
-                break;
-            case 6:
-                var animal = "Galo"
-                break;
-            case 7:
-                var animal = "Cão"
-                break;
-            case 8:
-                var animal = "Porco"
-                break;
-            case 9:
-                var animal = "Rato"
-                break;
-            case 10:
-                var animal = "Boi"
-                break;
-            case 11:
-                var animal = "Tigre"
-                break;
-
-            default:
-                var animal = "Resultado Inválido"
-                break;
-        }
-
-        res.innerHTML = `O seu animal no horóscopo chines é: ${animal}`
-
-
-    } else if (ano < anoLimite) {
-        res.innerHTML = `Ano fora do limite estabelecido! (${anoLimite})`
-    } 
-       else{
-        res.innerHTML = "Digite um ano válido"
+        calcularAnimal(ano);
     }
 }
